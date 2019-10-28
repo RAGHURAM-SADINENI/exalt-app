253602a4252793939b1141d709cafea5ca82d523
RAGHURAM SADINENI <sadineni.raghuram@gmail.com>
2019-10-28 17:38:06 +0530

    completed layout

--- exalt-app/src/app/pages/home/home.page.ts
+++ exalt-app/src/app/pages/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { news } from 'src/app/models/news';
+import { newsdata } from 'src/app/demodata/newsdata';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements OnInit {
+  firstitem:news;
+  newsdata:news[];
   constructor() {}
-
+  ngOnInit(){
+    this.firstitem=newsdata.shift();
+    this.newsdata=newsdata;
+  }
 }
